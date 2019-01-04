d611be0ec00f022f0e5bfd1173ed1b38af03ae21
Akshay Narayan <akshayn@mit.edu>
2019-01-04 21:29:04 +0000

    scripts: Add samplerate argument to experiment script

--- exp.py
+++ exp.py
@@ -8,9 +8,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--qdisc', type=str, dest='qdisc')
 parser.add_argument('--outdir', type=str, dest='outdir')
 parser.add_argument('--alg', type=str, dest='alg')
+parser.add_argument('--samplerate', type=int, dest='samplerate')
 parser.add_argument('--conns', type=int, dest='conns')
 
-nimbus = 'sudo ~/nimbus/target/release/nimbus --ipc=unix --use_switching=false --flow_mode=Delay 2> {}/nimbus.out'
+nimbus = 'sudo ~/nimbus/target/release/nimbus --ipc=unix --use_switching=false --flow_mode=Delay --bw_est_mode=false 2> {}/nimbus.out'
 bbr = 'sudo ~/bbr/target/release/bbr --ipc=unix 2> {}/bbr.out'
 osc = 'sudo python ~/portus/python/osc.py'
 algs = {
@@ -19,8 +20,8 @@ algs = {
     'osc': osc,
 }
 
-def remote_script(outdir, qdisc, alg, conns):
-    inbox = 'sudo ~/bundler/box/target/release/inbox --iface=10gp1 --handle_major={} --handle_minor=0x0 --port=28316 --sample_rate=128 2> {}/inbox.out'.format(qdisc, outdir)
+def remote_script(outdir, qdisc, alg, conns, samplerate):
+    inbox = 'sudo ~/bundler/box/target/release/inbox --iface=10gp1 --handle_major={} --handle_minor=0x0 --port=28316 --sample_rate={} 2> {}/inbox.out'.format(qdisc, samplerate, outdir)
     iperf = '~/iperf/src/iperf -s -p 5000 --reverse -i 1 -t 30 -P {} > {}/iperf-server.out'.format(conns, outdir)
     with open('remote.sh', 'w') as f:
         f.write('#!/bin/bash\n\n')
@@ -35,8 +36,8 @@ def remote_script(outdir, qdisc, alg, conns):
     sh.run('scp ./remote.sh 10.1.1.2:', shell=True)
     sh.Popen('ssh 10.1.1.2 ~/remote.sh' , shell=True)
 
-def local_script(outdir, conns):
-    outbox = 'sudo ~/bundler/box/target/release/outbox --filter "port 5000" --iface ingress --inbox 10.1.1.2:28316 --no_ethernet --sample_rate 128 > {}/outbox.out'.format(outdir)
+def local_script(outdir, conns, samplerate):
+    outbox = 'sudo ~/bundler/box/target/release/outbox --filter "port 5000" --iface ingress --inbox 10.1.1.2:28316 --no_ethernet --sample_rate {} > {}/outbox.out'.format(samplerate, outdir)
     iperf = 'iperf -c 10.1.1.2 -p 5000 --reverse -i 1 -P {} > {}/iperf-client.out'.format(conns, outdir)
 
     with open('local.sh', 'w') as f:
@@ -54,8 +55,8 @@ def local_script(outdir, conns):
     sh.run(mahimahi, shell=True)
 
 args = parser.parse_args()
-remote_script(args.outdir, args.qdisc, algs[args.alg], args.conns)
-local_script(args.outdir, args.conns)
+remote_script(args.outdir, args.qdisc, algs[args.alg], args.conns, args.samplerate)
+local_script(args.outdir, args.conns, args.samplerate)
 
 sh.run('ssh 10.1.1.2 sudo pkill inbox', shell=True)
 sh.run('ssh 10.1.1.2 sudo pkill nimbus', shell=True)
