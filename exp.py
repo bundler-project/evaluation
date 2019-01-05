77ecdcf8a35236ff61766537458e10505948a163
Akshay Narayan <akshayn@mit.edu>
2019-01-05 20:39:55 +0000

    scripts: Add option for running without bundler

--- exp.py
+++ exp.py
@@ -10,6 +10,7 @@ parser.add_argument('--outdir', type=str, dest='outdir')
 parser.add_argument('--alg', type=str, dest='alg')
 parser.add_argument('--samplerate', type=int, dest='samplerate')
 parser.add_argument('--conns', type=int, dest='conns')
+parser.add_argument('--no_bundler', action='store_false', dest='usebundler')
 
 nimbus = 'sudo ~/nimbus/target/release/nimbus --ipc=unix --use_switching=false --flow_mode=Delay --bw_est_mode=false 2> {}/nimbus.out'
 bbr = 'sudo ~/bbr/target/release/bbr --ipc=unix 2> {}/bbr.out'
@@ -20,30 +21,32 @@ algs = {
     'osc': osc,
 }
 
-def remote_script(outdir, qdisc, alg, conns, samplerate):
+def remote_script(use_bundler, outdir, qdisc, alg, conns, samplerate):
     inbox = 'sudo ~/bundler/box/target/release/inbox --iface=10gp1 --handle_major={} --handle_minor=0x0 --port=28316 --sample_rate={} 2> {}/inbox.out'.format(qdisc, samplerate, outdir)
     iperf = '~/iperf/src/iperf -s -p 5000 --reverse -i 1 -t 30 -P {} > {}/iperf-server.out'.format(conns, outdir)
     with open('remote.sh', 'w') as f:
         f.write('#!/bin/bash\n\n')
         f.write('rm -rf ~/{}\n'.format(outdir))
         f.write('mkdir -p ~/{}\n'.format(outdir))
-        f.write(inbox + ' &\n')
-        f.write('sleep 1\n')
-        f.write(alg.format(outdir) + ' &\n')
+        if use_bundler:
+            f.write(inbox + ' &\n')
+            f.write('sleep 1\n')
+            f.write(alg.format(outdir) + ' &\n')
         f.write(iperf + '\n')
 
     sh.run('chmod +x remote.sh', shell=True)
     sh.run('scp ./remote.sh 10.1.1.2:', shell=True)
     sh.Popen('ssh 10.1.1.2 ~/remote.sh' , shell=True)
 
-def local_script(outdir, conns, samplerate):
+def local_script(use_bundler, outdir, conns, samplerate):
     outbox = 'sudo ~/bundler/box/target/release/outbox --filter "port 5000" --iface ingress --inbox 10.1.1.2:28316 --no_ethernet --sample_rate {} > {}/outbox.out'.format(samplerate, outdir)
     iperf = 'iperf -c 10.1.1.2 -p 5000 --reverse -i 1 -P {} > {}/iperf-client.out'.format(conns, outdir)
 
     with open('local.sh', 'w') as f:
         f.write('#!/bin/bash\n\n')
-        f.write('sleep 1\n')
-        f.write(outbox + ' &\n')
+        if use_bundler:
+            f.write('sleep 1\n')
+            f.write(outbox + ' &\n')
         f.write('sleep 1\n')
         f.write('echo \'starting iperf client\'\n')
         f.write(iperf + '\n')
@@ -55,8 +58,8 @@ def local_script(outdir, conns, samplerate):
     sh.run(mahimahi, shell=True)
 
 args = parser.parse_args()
-remote_script(args.outdir, args.qdisc, algs[args.alg], args.conns, args.samplerate)
-local_script(args.outdir, args.conns, args.samplerate)
+remote_script(args.usebundler, args.outdir, args.qdisc, algs[args.alg], args.conns, args.samplerate)
+local_script(args.usebundler, args.outdir, args.conns, args.samplerate)
 
 sh.run('ssh 10.1.1.2 sudo pkill inbox', shell=True)
 sh.run('ssh 10.1.1.2 sudo pkill nimbus', shell=True)
