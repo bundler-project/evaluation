c1c4eed2eec15e416daac0efe0c8373e0e180799
Akshay Narayan <akshayn@mit.edu>
2019-01-08 17:22:25 +0000

    script: Log commit and arguments

--- exp.py
+++ exp.py
@@ -11,6 +11,7 @@ parser.add_argument('--alg', type=str, dest='alg')
 parser.add_argument('--samplerate', type=int, dest='samplerate')
 parser.add_argument('--conns', type=int, dest='conns')
 parser.add_argument('--no_bundler', action='store_false', dest='usebundler')
+parser.add_argument('--static_epoch', action='store_false', dest='dynamic_epoch')
 
 nimbus = 'sudo ~/nimbus/target/release/nimbus --ipc=unix --use_switching=false --flow_mode=Delay --bw_est_mode=false --pulse_size=0.01 2> {}/nimbus.out'
 bbr = 'sudo ~/bbr/target/release/bbr --ipc=unix 2> {}/bbr.out'
@@ -21,8 +22,8 @@ algs = {
     'osc': osc,
 }
 
-def remote_script(use_bundler, outdir, qdisc, alg, conns, samplerate):
-    inbox = 'sudo ~/bundler/box/target/release/inbox --iface=10gp1 --handle_major={} --handle_minor=0x0 --port=28316 --sample_rate={} 2> {}/inbox.out'.format(qdisc, samplerate, outdir)
+def remote_script(use_bundler, outdir, qdisc, alg, conns, samplerate, dynamic_samplerate):
+    inbox = 'sudo ~/bundler/box/target/release/inbox --iface=10gp1 --handle_major={} --handle_minor=0x0 --port=28316 --use_dynamic_sample_rate={} --sample_rate={} 2> {}/inbox.out'.format(qdisc, dynamic_samplerate, samplerate, outdir)
     iperf = '~/iperf/src/iperf -s -p 5000 --reverse -i 1 -t 60 -P {} > {}/iperf-server.out'.format(conns, outdir)
     with open('remote.sh', 'w') as f:
         f.write('#!/bin/bash\n\n')
@@ -66,10 +67,29 @@ sh.run('ssh 10.1.1.2 sudo pkill iperf', shell=True)
 sh.run('sudo pkill outbox', shell=True)
 sh.run('sudo pkill iperf', shell=True)
 
-print("starting")
-remote_script(args.usebundler, args.outdir, args.qdisc, algs[args.alg], args.conns, args.samplerate)
-local_script(args.usebundler, args.outdir, args.conns, args.samplerate)
+a = vars(args)
+commit = sh.check_output('git rev-parse HEAD', shell=True)
+commit = commit.decode('utf-8')[:-1]
+print('commit {}'.format(str(commit)))
+for k in sorted(a):
+    print(k, a[k])
+
+remote_script(
+    args.usebundler,
+    args.outdir,
+    args.qdisc,
+    algs[args.alg],
+    args.conns,
+    'true' if args.dynamic_epoch else 'false',
+    args.samplerate,
+)
+
+local_script(
+    args.usebundler,
+    args.outdir,
+    args.conns,
+    args.samplerate,
+)
+
 sh.run('scp 10.1.1.2:~/{0}/* ./{0}'.format(args.outdir), shell=True)
 sh.run('mm-graph {}/mahimahi.log 50'.format(args.outdir), shell=True)
-
-# impl dual mahimahi
