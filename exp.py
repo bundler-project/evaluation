b006b05d610f915472abd0683fe760c366f7fd0d
Akshay Narayan <akshayn@mit.edu>
2019-01-09 17:09:01 +0000

    script: Add argument to use three machines
    Using two machines cause no queue to build up at the inbox qdisc
    due to TCP small queues. Using three machines (note that the routing
    has to be set up properly) avoids this TSQ issue.

--- exp.py
+++ exp.py
@@ -13,6 +13,7 @@ parser.add_argument('--samplerate', type=int, dest='samplerate')
 parser.add_argument('--conns', type=int, dest='conns')
 parser.add_argument('--no_bundler', action='store_false', dest='usebundler')
 parser.add_argument('--static_epoch', action='store_false', dest='dynamic_epoch')
+parser.add_argument('--three_machines', action='store_true', dest='use_three_machines')
 
 nimbus = 'sudo ~/nimbus/target/release/nimbus --ipc=unix --use_switching=false --flow_mode=Delay --bw_est_mode=false --pulse_size=0.01 2> {}/nimbus.out'
 bbr = 'sudo ~/bbr/target/release/bbr --ipc=unix 2> {}/bbr.out'
@@ -23,11 +24,13 @@ algs = {
     'osc': osc,
 }
 
-def remote_script(time, use_bundler, outdir, qdisc, alg, conns, samplerate, dynamic_samplerate):
+def remote_script(time, use_bundler, three_machines, outdir, qdisc, alg, conns, samplerate, dynamic_samplerate):
     inbox = 'sudo ~/bundler/box/target/release/inbox --iface=10gp1 --handle_major={} --handle_minor=0x0 --port=28316 --use_dynamic_sample_rate={} --sample_rate={} 2> {}/inbox.out'.format(qdisc, dynamic_samplerate, samplerate, outdir)
     iperf = '~/iperf/src/iperf -s -p 5000 --reverse -i 1 -t {} -P {} > {}/iperf-server.out'.format(time, conns, outdir)
+
     print(inbox)
     print(iperf)
+
     with open('remote.sh', 'w') as f:
         f.write('#!/bin/bash\n\n')
         f.write('rm -rf ~/{}\n'.format(outdir))
@@ -36,15 +39,36 @@ def remote_script(time, use_bundler, outdir, qdisc, alg, conns, samplerate, dyna
             f.write(inbox + ' &\n')
             f.write('sleep 1\n')
             f.write(alg.format(outdir) + ' &\n')
-        f.write(iperf + '\n')
+        if not three_machines:
+            f.write(iperf + '\n')
 
     sh.run('chmod +x remote.sh', shell=True)
     sh.run('scp ./remote.sh 10.1.1.2:', shell=True)
-    sh.Popen('ssh 10.1.1.2 ~/remote.sh' , shell=True)
 
-def local_script(use_bundler, outdir, conns, samplerate):
-    outbox = 'sudo ~/bundler/box/target/release/outbox --filter "port 5000" --iface ingress --inbox 10.1.1.2:28316 --no_ethernet --sample_rate {} 2> {}/outbox.out'.format(samplerate, outdir)
-    iperf = 'iperf -c 10.1.1.2 -p 5000 --reverse -i 1 -P {} > {}/iperf-client.out'.format(conns, outdir)
+    if three_machines:
+        with open('remote.sh', 'w') as f:
+            f.write('#!/bin/bash\n\n')
+            f.write('rm -rf ~/{}\n'.format(outdir))
+            f.write('mkdir -p ~/{}\n'.format(outdir))
+            f.write(iperf + '\n')
+        sh.run('chmod +x remote.sh', shell=True)
+        sh.run('scp ./remote.sh 10.1.1.5:', shell=True)
+
+    sh.Popen('ssh 10.1.1.2 ~/remote.sh' , shell=True)
+    sh.Popen('ssh 192.168.1.5 ~/remote.sh' , shell=True)
+
+def local_script(use_bundler, three_machines, outdir, conns, samplerate):
+    outbox = 'sudo ~/bundler/box/target/release/outbox --filter "port 5000" --iface ingress --inbox 10.1.1.2:28316 {} --sample_rate {} 2> {}/outbox.out'.format(
+        "--no_ethernet",
+        samplerate,
+        outdir,
+    )
+    iperf = 'iperf -c {} -p 5000 --reverse -i 1 -P {} > {}/iperf-client.out'.format(
+        '192.168.1.5' if three_machines else '10.1.1.2',
+        conns,
+        outdir,
+    )
+
     print(outbox)
     print(iperf)
     with open('local.sh', 'w') as f:
@@ -68,6 +92,7 @@ sh.run('ssh 10.1.1.2 sudo pkill inbox', shell=True)
 sh.run('ssh 10.1.1.2 sudo pkill nimbus', shell=True)
 sh.run('ssh 10.1.1.2 sudo pkill bbr', shell=True)
 sh.run('ssh 10.1.1.2 sudo pkill iperf', shell=True)
+sh.run('ssh 192.168.1.5 sudo pkill iperf', shell=True)
 sh.run('sudo pkill outbox', shell=True)
 sh.run('sudo pkill iperf', shell=True)
 
@@ -81,6 +106,7 @@ for k in sorted(a):
 remote_script(
     args.time,
     args.usebundler,
+    args.use_three_machines,
     args.outdir,
     args.qdisc,
     algs[args.alg],
@@ -91,10 +117,14 @@ remote_script(
 
 local_script(
     args.usebundler,
+    args.use_three_machines,
     args.outdir,
     args.conns,
     args.samplerate,
 )
 
 sh.run('scp 10.1.1.2:~/{0}/* ./{0}'.format(args.outdir), shell=True)
+if args.use_three_machines:
+    sh.run('scp 192.168.1.5:~/{0}/* ./{0}'.format(args.outdir), shell=True)
+
 sh.run('mm-graph {}/mahimahi.log 50'.format(args.outdir), shell=True)
