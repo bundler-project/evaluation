b7ee2dc1261b8e29d5e81a47999c1feb657410a5
Akshay Narayan <akshayn@mit.edu>
2019-02-01 18:55:19 +0000

    scripts: Run multiple servers

--- run-multiple-server.sh
+++ run-multiple-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in `seq 1 $1`; do
-    ./empirical-traffic-gen/bin/server -t reno -p $((5000 + $i)) >> /dev/null &
+for i in `seq 0 $2`; do
+    ./empiricial-traffic-gen/bin/server -t reno -p $(($1 + $i)) >> /dev/null &
 done
