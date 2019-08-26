e1046b8df280044825c4999b7962726fa35eede9
Akshay Narayan <akshayn@mit.edu>
2019-08-26 09:47:26 -0400

    ec2 setup works

--- cloud/src/main.rs
+++ cloud/src/main.rs
@@ -32,21 +32,17 @@ fn get_iface_name(node: &Session) -> Result<String, Error> {
         })
 }
 
+fn install_basic_packages(ssh: &Session) -> Result<(), Error> {
+    ssh.cmd("sudo apt update").map(|(_, _)| ())?;
+    ssh.cmd("sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install -y build-essential git automake autoconf libtool")
+        .map(|(_, _)| ())
+}
+
 fn get_tools(ssh: &Session) -> Result<(), Error> {
     ssh.cmd("git clone --recursive https://github.com/bundler-project/tools")
-        .map(|(out, err)| {
-            println!("clone_stdout: {}", out);
-            println!("clone_stderr: {}", err);
-        })?;
+        .map(|(_, _)| ())?;
 
-    println!("press enter to continue");
-    let mut buf = String::new();
-    std::io::stdin().read_line(&mut buf)?;
-
-    ssh.cmd("make -C tools").map(|(out, err)| {
-        println!("make_stdout: {}", out);
-        println!("make_stderr: {}", err);
-    })
+    ssh.cmd("make -C tools").map(|(_, _)| ())
 }
 
 fn main() -> Result<(), Error> {
@@ -57,22 +53,18 @@ fn main() -> Result<(), Error> {
 
     let m0 = MachineSetup::default()
         .region(opt.send_region.clone())
-        .instance_type("t3.small")
+        .instance_type("t3.medium")
         .setup(|ssh| {
-            // also need mahimahi to do mm-delay 0 in front of sending iperf
-            ssh.cmd("sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install -y build-essential git automake autoconf libtool mahimahi")
-                .map(|(_, _)| ())?;
+            install_basic_packages(ssh).map_err(|e| e.context("m0 apt install failed"))?;
             println!("m0 finished apt install");
-
             get_tools(ssh)
         });
 
     let m1 = MachineSetup::default()
         .region(opt.recv_region.clone())
-        .instance_type("t3.small")
+        .instance_type("t3.medium")
         .setup(|ssh| {
-            ssh.cmd("sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install -y build-essential git automake autoconf libtool")
-                .map(|(_, _)| ())?;
+            install_basic_packages(ssh).map_err(|e| e.context("m1 apt install failed"))?;
             println!("m1 finished apt install");
 
             get_tools(ssh)
@@ -85,12 +77,14 @@ fn main() -> Result<(), Error> {
         let sender_ip = &vms.get("sender").expect("get sender").public_ip;
         let receiver_ip = &vms.get("sender").expect("get sender").public_ip;
 
-        let sender = vms.get("sender")
+        let sender = vms
+            .get("sender")
             .expect("get sender")
             .ssh
             .as_ref()
             .expect("sender ssh connection");
-        let recevr = vms.get("receiver")
+        let recevr = vms
+            .get("receiver")
             .expect("get receiver")
             .ssh
             .as_ref()
@@ -98,42 +92,48 @@ fn main() -> Result<(), Error> {
         let sender_iface = get_iface_name(sender)?;
         let receiver_iface = get_iface_name(recevr)?;
 
-        // start outbox
-        recevr.cmd(&format!("cd ~/tools/bundler && sudo screen -d -m bash -c \"./target/debug/outbox --filter=\\\"src portrange 5000-6000\\\" --iface={} --inbox {}:28316 --sample-rate=64", 
-                receiver_iface,
-                sender_ip,
-            ))
-            .map(|(out, err)| {
-                println!("outbox_out: {}", out);
-                println!("errbox_err: {}", err);
-            })?;
+        // don't bother trying to actually do anything, just wait for now
+        println!(
+            "success, ready: {} =>{} ; {} => {}",
+            sender_ip, sender_iface, receiver_ip, receiver_iface
+        );
+        std::thread::sleep(std::time::Duration::from_secs(120));
+        //// start outbox
+        //recevr.cmd(&format!("cd ~/tools/bundler && sudo screen -d -m bash -c \"./target/debug/outbox --filter=\\\"src portrange 5000-6000\\\" --iface={} --inbox {}:28316 --sample-rate=64",
+        //        receiver_iface,
+        //        sender_ip,
+        //    ))
+        //    .map(|(out, err)| {
+        //        println!("outbox_out: {}", out);
+        //        println!("errbox_err: {}", err);
+        //    })?;
 
-        // start iperf receiver
-        recevr.cmd("cd ~/tools/iperf && screen -d -m bash -c \"./src/iperf -s -p 5001\"").map(|_| ())?;
+        //// start iperf receiver
+        //recevr.cmd("cd ~/tools/iperf && screen -d -m bash -c \"./src/iperf -s -p 5001\"").map(|_| ())?;
 
-        // start inbox
-        sender
-            .cmd(&format!("cd ~/tools/bundler && sudo screen -d -m bash -c \"./target/debug/inbox --iface={} --port 28316 --sample_rate=128 --qtype={} --buffer={}\"", 
-                          sender_iface,
-                          opt.inbox_qtype,
-                          opt.inbox_qlen,
-                          ))
-            .map(|(out, err)| {
-                println!("inbox_out: {}", out);
-                println!("inbox_err: {}", err);
-            })?;
+        //// start inbox
+        //sender
+        //    .cmd(&format!("cd ~/tools/bundler && sudo screen -d -m bash -c \"./target/debug/inbox --iface={} --port 28316 --sample_rate=128 --qtype={} --buffer={}\"",
+        //                  sender_iface,
+        //                  opt.inbox_qtype,
+        //                  opt.inbox_qlen,
+        //                  ))
+        //    .map(|(out, err)| {
+        //        println!("inbox_out: {}", out);
+        //        println!("inbox_err: {}", err);
+        //    })?;
 
-        // start nimbus
-        sender.cmd(&format!("cd ~/tools/nimbus && sudo ./target/debug/nimbus --ipc=unix --use_switching=true --loss_mode=Bundle --delay_mode=Nimbus --flow_mode=XTCP --bundler_qlen_alpha=100 --bundler-qlen-beta=10000 --bundler_qlen_target=100")).map(|(out, err)| {
-            println!("nimbus_out: {}", out);
-            println!("nimbus_err: {}", err);
-        })?;
+        //// start nimbus
+        //sender.cmd(&format!("cd ~/tools/nimbus && sudo ./target/debug/nimbus --ipc=unix --use_switching=true --loss_mode=Bundle --delay_mode=Nimbus --flow_mode=XTCP --bundler_qlen_alpha=100 --bundler-qlen-beta=10000 --bundler_qlen_target=100")).map(|(out, err)| {
+        //    println!("nimbus_out: {}", out);
+        //    println!("nimbus_err: {}", err);
+        //})?;
 
-        // start iperf sender inside mm-delay 0
-        sender.cmd(&format!("cd ~/tools/iperf && ./src/iperf -c {} -p 5001 -t 30 -i 1", receiver_ip)).map(|(out, err)| {
-            println!("iperf sender_out: {}", out);  
-            println!("iperf sender_err: {}", err);  
-        })?;
+        //// start iperf sender inside mm-delay 0
+        //sender.cmd(&format!("cd ~/tools/iperf && ./src/iperf -c {} -p 5001 -t 30 -i 1", receiver_ip)).map(|(out, err)| {
+        //    println!("iperf sender_out: {}", out);
+        //    println!("iperf sender_err: {}", err);
+        //})?;
 
         Ok(())
     })?;
